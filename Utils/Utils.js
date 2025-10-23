48bb279d86b70cfe7a7559e7bc81bcf50b1725ef
MiguelGonSe <gonzalezsegurom@gmail.com>
2025-10-23 10:17:53 +0200

    ✅ New Css y 🔄 Update Utils
    Creación del Css
    Actualización del Utils con las Scene

--- Utils/Utils.js
+++ Utils/Utils.js
@@ -31,3 +31,17 @@ export function groupBy(array, keyFunc) {
         return acc;
     }, {});
 }
+
+/**
+  * Permite mostrar u ocultar un elemento (escena) de la pantalla.
+  * Selecciona todos los elementos con la clase scene y les remueve la clase active
+  * Selecciona el elemento con el id especificado y le añade la clase active
+  * @param {number} id - identificador del elemento.
+  */
+ 
+export function showScene(id) {
+    document.querySelectorAll('.scene').forEach(
+        element => element.classList.remove('active')
+    );
+    document.getElementById(id).classList.add('active');
+}

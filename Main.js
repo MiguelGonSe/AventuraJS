91beb76202ccc82b45331acf33ba37095183d898
MiguelGonSe <gonzalezsegurom@gmail.com>
2025-10-24 18:12:08 +0200

    🔄 Update index y main
    Ajustes en index y main para mayor organización del código. Y preparación del market.

--- Main.js
+++ Main.js
@@ -5,62 +5,78 @@ import { agruparPorNivel, batalla, mostrarRanking } from './Modules/Ranking.js';
 import { showScene } from './Utils/Utils.js';
 
 let jugador;
+let imagen = "";
 
-document.getElementById('btnL').addEventListener('click', function () {
-    jugador = new Personaje('Elfo', 50, 20, 0, 200);
-    mostrarStats('IMG/legolas.webp');
-});
-
-document.getElementById('btnG').addEventListener('click', function () {
-    jugador = new Personaje('Guerrero', 60, 10, 0, 200);
-    mostrarStats('IMG/guerrero.png');
-});
-
-document.getElementById('btnO').addEventListener('click', function () {
-    jugador = new Personaje('Orco', 40, 30, 0, 200);
-    mostrarStats('IMG/orco.png');
-});
-
-document.getElementById('back_choose_player').addEventListener('click', function () {
-    document.getElementById('choose_player').classList.remove('scene');
+// FUNCIONES DE CAMBIO DE ESCENA
+function showChoosePlayer() {
     document.getElementById('choose_player').classList.add('scene_active');
-    document.getElementById('look_stats').classList.remove('scene_active');
+    document.getElementById('choose_player').classList.remove('scene');
     document.getElementById('look_stats').classList.add('scene');
-});
+    document.getElementById('look_stats').classList.remove('scene_active');
+}
 
-function mostrarStats(imagen) {
+function showStats() {
     document.getElementById('choose_player').classList.remove('scene_active');
     document.getElementById('choose_player').classList.add('scene');
     document.getElementById('look_stats').classList.remove('scene');
     document.getElementById('look_stats').classList.add('scene_active');
-    
-    document.getElementById('chosenImg').src = imagen;
+}
 
+function showMarketScene() {
+    document.getElementById('look_stats').classList.remove('scene_active');
+    document.getElementById('look_stats').classList.add('scene');
+    document.getElementById('look_market').classList.remove('scene');
+    document.getElementById('look_market').classList.add('scene_active');
+}
+
+// ELECCIÓN DE JUGADOR
+document.getElementById('btnL').addEventListener('click', function () {
+    jugador = new Personaje('Elfo', 50, 20, 0, 200);
+    imagen = 'IMG/legolas.webp';
+    mostrarStats(imagen);
+});
+
+document.getElementById('btnG').addEventListener('click', function () {
+    jugador = new Personaje('Guerrero', 60, 10, 0, 200);
+    imagen = 'IMG/guerrero.png';
+    mostrarStats(imagen);
+});
+
+document.getElementById('btnO').addEventListener('click', function () {
+    jugador = new Personaje('Orco', 40, 30, 0, 200);
+    imagen = 'IMG/orco.png';
+    mostrarStats(imagen);
+});
+
+document.getElementById('back_choose_player').addEventListener('click', showChoosePlayer);
+
+// STATS
+function mostrarStats(imagen) {
+    showStats();
+    document.getElementById('chosenImg').src = imagen;
     document.getElementById('vida').textContent = jugador.vida;
     document.getElementById('ataque').textContent = jugador.ataque;
     document.getElementById('defensa').textContent = jugador.defensa;
     document.getElementById('puntos').textContent = jugador.puntos;
+    document.getElementById('monedas').textContent = jugador.monedas;
 }
 
-function mostrarMarket() {
+// MARKET
+document.getElementById('ir_market').addEventListener('click', function () {
+    const inputNombre = document.getElementById('nombre');
+    const nombreJugador = inputNombre.value.trim() || jugador.nombre;
+    const monedasJugador = jugador.monedas;
+    mostrarMarket(imagen, nombreJugador, monedasJugador);
+    const marketItemsDiv = document.getElementById('marketItems');
+    marketItemsDiv.textContent = "";
+    objetos.forEach(obj => {
+        marketItemsDiv.textContent += obj.mostrarInfo();
+    });
+});
 
+function mostrarMarket(imagen, nombre, monedas) {
+    showMarketScene();
+    document.querySelector('#look_market #chosenImg').src = imagen;
+    document.querySelector('#look_market #nombre').textContent = nombre;
+    document.querySelector('#look_market #monedas').textContent = monedas;
 }
-
-// const jugador = new Personaje('Elfo', 60, 10, 0, 200);
-// jugador.añadirProducto(objetos[0]);
-
-// const jugadores = [jugador, jugador2, jugador3];
-// const jefe = new Jefe('Dragón', 50, 50, 300, 'Fuego Infernal');
-
-
-// console.log(jugador.mostrarPersonaje());
-// console.log(jefe.presentar());
-// console.log(jefe.usarHabilidad());
-
-
-// console.log(batalla(jugador, jefe));
-
-// console.log("🎖 Clasificación por nivel");
-// console.log(agruparPorNivel(jugadores, 40));
-
-// console.log(mostrarRanking(jugadores));
